d0f8ecdacf43370d7acd6594d74f7929bb7d78b3
Arif Mehmood <arifmehmood9516@gmail.com>
2023-07-23 01:14:50 +0100

    Initial Commit

---
+++ src/main/java/com/appsdeveloper/photoapp/api/gateway/GlobalFiltersConfiguration.java
@@ -0,0 +1,47 @@
+package com.appsdeveloper.photoapp.api.gateway;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import reactor.core.publisher.Mono;
+
+@Configuration
+public class GlobalFiltersConfiguration {
+    final Logger logger = LoggerFactory.getLogger(GlobalFiltersConfiguration.class);
+
+    @Order(2)
+    @Bean
+    public GlobalFilter secondPreFilter(){
+        return (exchange, chain) -> {
+            logger.info("**** My second global pre filter...");
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                logger.info("**** My second post filter...");
+            }));
+        };
+    }
+
+    @Order(1)
+    @Bean
+    public GlobalFilter ThirdPreFilter(){
+        return (exchange, chain) -> {
+            logger.info("**** My third global pre filter...");
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                logger.info("**** My third post filter...");
+            }));
+        };
+    }
+
+    @Order(3)
+    @Bean
+    public GlobalFilter fourthPreFilter(){
+        return (exchange, chain) -> {
+            logger.info("**** My fourth global pre filter...");
+            return chain.filter(exchange).then(Mono.fromRunnable(()->{
+                logger.info("**** My fourth post filter...");
+            }));
+        };
+    }
+}
